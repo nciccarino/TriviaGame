9e6fcd74de9ab8384bc740f31b0db791f0868018
Nicholas A Ciccarino <nciccarino@gmail.com>
2017-06-01 23:58:03 -0400

    most game functionality

---
+++ assets/javascript/app.js
@@ -0,0 +1,74 @@
+//---------------Attempt 1---------------------------------------------------------------------------
+
+// //------------Global Variables--------------------------------
+
+// var correct = ["Mississippi", "Austin", "RhodeIsland", "Minnesota", "Russia", "NewHampshire"];
+
+// var clicked = []; //need to create a blank spot if left unclicked
+
+// //------------Functions---------------------------------------
+
+// $(document).ready(function(){
+
+// //------------Data Gathering----------------------------------
+
+//   $("#doneButton").on("click", function(){
+//     var selected = $("input[type=radio]:checked");
+//     $.each(selected, function(){
+//       clicked[this.id] = this.value;
+//       console.log(this)
+//       console.log(this.value)
+//       var len = correct.length; 
+//       var i = 0;
+//       for (i; i < len; i++) {
+//         if ($.inArray(correct[i], clicked)== -1) {
+//           console.log("incorrect");
+//         } else {
+//           console.log("correct"); 
+//         }
+//       }
+//     })
+//   })
+
+// }); //end 
+
+//-----------------End Attempt 1-------------------------------------------------------------------------------
+
+
+function none (){
+  var none = $("input:radio[value=true]:not(:checked)");
+    console.log(none);
+    none = none.length; 
+    $("#unanswered").append(none);
+}
+
+$("#doneButton").on("click", function(){
+  var correct = $("input:radio[value=true]:checked");
+    console.log(correct);
+    correct = correct.length;
+    $("#right").append(correct);
+  var incorrect = $("input:radio[value=false]:checked"); 
+    console.log(incorrect);
+    incorrect = incorrect.length;
+    $("#wrong").append(incorrect); 
+  none();
+});
+
+
+//   var none = $("input:radio[value=true]:not(:checked)").length;
+
+//   var correct = $("input:radio[value=true]:checked");
+//   var correctAnswer = correct.length
+
+//   var incorrect = $("input:radio[value=false]:checked"); 
+//   var incorrectAnswer = incorrect.length
+
+//   var fix = none - incorrect.length; 
+    
+
+
+// $("#doneButton").on("click", function(){
+//     $("#right").append(correctAnswer);
+//     $("#wrong").append(incorrectAnswer); 
+//     $("#unanswered").append(fix);
+// });
